659e173d8deb29247107a3c406df003e467c00d5
FantasyRL <1845983502@qq.com>
2025-11-12 15:23:39 +0800

    feat: service层添加供handler层调用的方法

--- internal/host/application/template.go
+++ internal/host/application/template.go
@@ -1,1 +1,11 @@
 package application
+
+import (
+	"github.com/FantasyRL/go-mcp-demo/api/model/api"
+	"github.com/FantasyRL/go-mcp-demo/pkg/gorm-gen/model"
+)
+
+func (h *Host) TemplateLogic(req *api.TemplateRequest) (*model.Users, error) {
+	// 这里编排你的业务逻辑
+	return &model.Users{}, nil
+}
